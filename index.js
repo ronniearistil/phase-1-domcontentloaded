905ad866cf57dd85ba4c2291c285e45c7dafbfe2
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-09-18 17:10:05 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+  });
+  
